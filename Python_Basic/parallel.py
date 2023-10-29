d914ae30765c05e0f26e72d67931efd55b38cf48
vietgym <vietgym007@gmail.com>
2023-10-29 22:39:40 +0700

    feat: upload file parallel.py

---
+++ Python_Basic/parallel.py
@@ -0,0 +1,16 @@
+#parallel (song song): thực hiện nhiều công việc cùng lúc thay vì thực hiện tuần tự
+
+#Multithreading dùng threading để tạo & quản lý các luồng (threads) riêng biệt
+#MultiprocessingL dùng multiprocessing tạo các tiến trình riêng biệt, phù hợp tính toán nặng
+#Asyncio dùng asyncio lập trình đồng bộ doroutine và event loop
+#Thư viện như concurrent.futures, joblib, Ray
+import concurrent.futures
+def process_data(data):
+    result = data ** 2
+    print(f"Xử lý dữ liệu {data}: Kết quả = {result}")
+data = [1, 2, 3, 4, 5]
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(process_data, data)
+
+
+
