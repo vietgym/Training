7f4b851118be4eedcf6a6a2ca2da7ce751154630
vietgym <vietgym007@gmail.com>
2023-10-30 14:10:02 +0700

    feat: up file list.py

--- Python_Basic/parallel.py
+++ Python_Basic/parallel.py
@@ -1,7 +1,8 @@
-#parallel (song song): thực hiện nhiều công việc cùng lúc thay vì thực hiện tuần tự
+#parallel (song song): thực hiện nhiều công việc cùng lúc thay vì thực hiện tuần tự. Tính toán trên nhiều máy tính hoặc lõi cpu cùng lúc
 
 #Multithreading dùng threading để tạo & quản lý các luồng (threads) riêng biệt
 #MultiprocessingL dùng multiprocessing tạo các tiến trình riêng biệt, phù hợp tính toán nặng
+from multiprocessing import Pool
 #Asyncio dùng asyncio lập trình đồng bộ doroutine và event loop
 #Thư viện như concurrent.futures, joblib, Ray
 import concurrent.futures
