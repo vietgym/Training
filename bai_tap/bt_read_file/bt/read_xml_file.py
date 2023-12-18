74da1e6e0142bac68af3c31a2e49f457260cddde
vietgym <vietgym007@gmail.com>
2023-12-18 13:01:51 +0700

    fix: reupload read xml file

--- bai_tap/bt_read_file/bt/read_xml_file.py
+++ bai_tap/bt_read_file/bt/read_xml_file.py
@@ -1,31 +1,34 @@
+import xml.etree.ElementTree as ET
 import json
-import fitz
+
+
+def xml_to_dict(element):
+    if len(element) == 0:
+        return element.text
+    result = {}
+    for child in element:
+        child_data = xml_to_dict(child)
+        if child.tag in result:
+            if type(result[child.tag]) is list:
+                result[child.tag].append(child_data)
+            else:
+                result[child.tag] = [result[child.tag], child_data]
+        else:
+            result[child.tag] = child_data
+    return result
 
 
 def extract_xml_info(xml_path):
-    xml_info = {}
-
-    xml_document = fitz.open(xml_path)
-
-    num_page = xml_document.page_count
-    xml_info["num_pages"] = num_page
-
-    page_info_list = []
-    for page_number in range(num_page):
-        page = xml_document[page_number]
-        page_info = {
-            "page_number": page_number + 1,
-            "text": page.get_text("text"),
-        }
-        page_info_list.append(page_info)
-
-    xml_info["pages"] = page_info_list
-
-    xml_document.close()
-
-    return xml_info
+    try:
+        tree = ET.parse(xml_path)
+        root = tree.getroot()
+        data_dict = {root.tag: xml_to_dict(root)}
+        json_data = json.dumps(data_dict, indent=2)
+        return json_data
+    except Exception as e:
+        return f"Error converting XML to JSON: {str(e)}"
 
 
 xml_path = r"D:\git_training\unittest\bt\769793.xml"
 xml_info = extract_xml_info(xml_path)
-print(json.dumps(xml_info, indent=2))
+print(xml_info)
